541c729504d7517f82d0cc47a471841756cd8e15
liber09 <linda.bergsangel@hotmail.com>
2023-03-25 11:30:42 +0100

    Select difficulty

--- FastTyper/Controllers/MainViewController.swift
+++ FastTyper/Controllers/MainViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var selectedDifficulty: Int = 0
+    
+    @IBOutlet weak var difficulty: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -21,7 +25,31 @@ class ViewController: UIViewController {
     }
     
     @IBAction func ShowHowToPlay(_ sender: Any) {
+        let howToPlayStoryboard = UIStoryboard(name: "HowToPlay",bundle: nil)
+        let howToVc = howToPlayStoryboard.instantiateViewController(withIdentifier: "HowToPlayViewController")
+        present(howToVc, animated: true, completion: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+           
+            if let gameViewController = segue.destination as? GameViewController{
+              gameViewController.selectedDifficulty = selectedDifficulty
+                
+            }
+        }
+    
+    @IBAction func selectDifficulty(_ sender: UISegmentedControl) {
+        switch(difficulty.selectedSegmentIndex){
+        case 0:
+            selectedDifficulty = 5
+        case 1:
+            selectedDifficulty = 4
+        case 2:
+            selectedDifficulty = 3
         
+        default:
+            selectedDifficulty = 5
+        }
     }
 }
 
---
+++ FastTyper/Model/Gameplay.swift
@@ -0,0 +1,12 @@
+//
+//  Gameplay.swift
+//  FastTyper
+//
+//  Created by Linda Bergsängel on 2023-03-25.
+//
+
+import Foundation
+
+class Game{
+    
+}
